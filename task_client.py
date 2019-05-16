09d5f09b117fbd2dd22f0d17350a0a82f10ee150
Alexander_Ryabykh <gedsil32@yandex.ru>
2019-05-16 12:07:56 +0400

    -исправлено отправление probe-сообщения от сервера, -сервер получает presense-сообщения от клиента в ответ, -реализован бызовый вариант работы через sys, без try-except

--- task_client.py
+++ task_client.py
@@ -2,9 +2,29 @@
 import json
 from socket import *
 import datetime
+import sys
+import re
+
+PATTERNS = [re.compile('\d+\.\d+\.\d+\.\d+'),
+            re.compile('\d\d\d\d')]
 
 s = socket(AF_INET, SOCK_STREAM)  # Создать сокет TCP
-s.connect(('localhost', 8007))   # Соединиться с сервером
+
+CONFIG = {'addr': None,
+          'port': None}
+
+if re.match(PATTERNS[0], sys.argv[1]):
+    CONFIG['addr'] = sys.argv[1]
+else:
+    CONFIG['addr'] = '127.0.0.7'
+
+if re.match(PATTERNS[1], sys.argv[2]):
+    CONFIG['port'] = int(sys.argv[2])
+else:
+    CONFIG['port'] = 8007
+
+s.connect((CONFIG['addr'], CONFIG['port']))   # Соединиться с сервером
+
 message_dict = {
         "action": "presence",
         "time": str(datetime.datetime.utcnow()),
@@ -19,7 +39,10 @@ msg = json.dumps(message_dict)
 
 data = s.recv(100000000)
 print('Сообщение от сервера: ', data.decode('utf-8'), ', длиной ', len(data), ' байт')
-if data['action'] == 'probe':
+
+data_decoded = json.loads(data)
+if data_decoded['action'] == 'probe':
     s.send(msg.encode('utf-8'))
     print('Сообщение отправлено')
-s.close()
+
+s.close() # а зачем в клиенте закрывать сокет? скрипт срабатывает и без закрытия
--- task_server.py
+++ task_server.py
@@ -2,9 +2,28 @@
 import json
 from socket import *
 import datetime
+import sys
+import re
+
+PATTERNS = [re.compile('\d+\.\d+\.\d+\.\d+'),
+            re.compile('\d\d\d\d')]
 
 s = socket(AF_INET, SOCK_STREAM)  # Создает сокет TCP
-s.bind(('', 8007))                # Присваивает порт 8007
+
+CONFIG = {'addr': None,
+          'port': None}
+
+if re.match(PATTERNS[0], sys.argv[1]):
+    CONFIG['addr'] = sys.argv[1]
+else:
+    CONFIG['addr'] = '127.0.0.7'
+
+if re.match(PATTERNS[1], sys.argv[2]):
+    CONFIG['port'] = int(sys.argv[2])
+else:
+    CONFIG['port'] = 8007
+
+s.bind((CONFIG['addr'], CONFIG['port']))                # Присваивает порт 8007
 s.listen(5)                       # Переходит в режим ожидания запросов;
                                   # одновременно обслуживает не более
                                   # 5 запросов.
@@ -20,20 +39,25 @@ def build_error_for_client(code, error):
 
 
 def probe_client(client):
-    probe_msg = {
-        "action": "probe",
-        "time": str(datetime.datetime.utcnow()),
+    probe = {
+        'action': 'probe',
+        'time': str(datetime.datetime.utcnow()),
     }
+    probe_msg = json.dumps(probe)
     client.send(probe_msg.encode('utf-8'))
-    client.close()
+    # client.close() # если закрываю клиент здесь, сервер перестанет работать
 
 
 while True:
     client, addr = s.accept()
+
+
+    probe_client(client)
+
     data = client.recv(1000000)
     print('Сообщение: ', data.decode('utf-8'), ', было отправлено клиентом: ', addr)
-
     data_dict = json.loads(data)
+    action = data_dict['action']
 
     if 'action' not in data_dict:
         code = 400
@@ -42,10 +66,7 @@ while True:
         client.send(response_msg_str.encode('utf-8'))
         client.close()
 
-    probe_client(client)
-    action = data_dict['action']
-
     if action == 'presence':
         print('пользователь в сети')
 
-
+    # client.close() # нужно ли здесь закрыть клиент-сокет в конце работы?
